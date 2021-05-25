819ba600a9acabb2d3771069a241fdbff56c5e45
lex <lex@poltor.ru>
2021-05-25 23:38:44 +0200

    les04

---
+++ lesson05/lesson05-1.py
@@ -0,0 +1,19 @@
+# 1. Создать программно файл в текстовом формате, записать в него построчно данные, вводимые
+# пользователем. Об окончании ввода данных свидетельствует пустая строка.
+
+new_file = open(r"somefile.txt", "w+")
+while True:
+    user_input = str(input("Введите строку, пустая строка - выход >>>\n"))
+    if user_input == str(""):
+        break
+    else:
+        if new_file.writable():
+            new_file.writelines(user_input + "\n")
+        else:
+            "Файл недоступен для записи"
+new_file.close()
+
+new_file = open(r"somefile.txt", "r")
+print(f"Записано в файл {new_file.name} следующее содержимое:")
+print(new_file.read())
+new_file.close()
